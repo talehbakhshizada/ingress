3e53b95fb808222d522329be11249fca66f3d099
Taleh Bakhshizada <tbakhshizada@gmail.com>
2023-08-21 02:26:36 +0400

    "services done"

---
+++ ms-publisher/src/main/java/az/company/mspublisher/MsPublisherApplication.java
@@ -0,0 +1,13 @@
+package az.company.mspublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPublisherApplication.class, args);
+    }
+
+}
---
+++ ms-publisher/src/test/java/az/company/mspublisher/MsPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package az.company.mspublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
